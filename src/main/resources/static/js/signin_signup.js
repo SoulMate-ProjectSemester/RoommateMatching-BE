c062c11a28bd335fa6e034d212fd2c31a31504b8
High-Quality-Coffee <125748258+High-Quality-Coffee@users.noreply.github.com>
2024-03-10 03:21:19 +0900

    login 기능 구현완료

--- src/main/resources/static/js/signin_signup.js
+++ src/main/resources/static/js/signin_signup.js
@@ -2,3 +2,55 @@ document.querySelector('.img__btn').addEventListener('click', function() {
     document.querySelector('.cont').classList.toggle('s--signup');
 });
 
+function login() {
+    // Get input values
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    if(!username) { alert("아이디를 다시 입력하세요"); return; }
+    if(!password) {alert("비밀번호를 다시 입력하세요"); return; }
+
+    // Prepare data to send
+    var data = {
+        loginId: username,
+        password: password
+    };
+
+    //set axios instance
+    const instance = axios.create({
+        baseURL: "http://localhost:8080",
+        timeout: 5000,
+        headers: {
+            "Cache-Control": "no-cache",
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+        },
+        responseType: "json",
+    });
+
+    // Send POST request using axios
+    try {
+        const response=instance.post("/api/member/login",{
+            loginId: username,
+            password: password
+        });
+        console.log(response);
+        //promise에서 내가 원하는 value 값 받기
+        response.then(response => {
+            // Access the 'data' property from the resolved value
+            const responseData = response.data.resultCode;
+            if(responseData=='SUCCESS'){
+                window.location.href='http://localhost:8080';
+                window.localStorage.setItem('token',response.data.data.accessToken);
+            }
+        }).catch(error => {
+            // Handle errors if the Promise is rejected
+            console.error('Error occurred:', error);
+            alert('아이디 혹은 비밀번호를 다시한번 확인하세요.');
+        });
+
+    }catch (error){
+        console.error("로그인 중 에러:", error);
+    }
+
+}

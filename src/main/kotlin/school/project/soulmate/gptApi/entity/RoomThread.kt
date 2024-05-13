a2998a78e0fa4e2cc729810e659e490dd5f64f87
vkflco08 <vkflco08@naver.com>
2024-05-13 15:29:45 +0900

    feat create gpt api entity

--- src/main/kotlin/school/project/soulmate/gptApi/entity/RoomThread.kt
+++ src/main/kotlin/school/project/soulmate/gptApi/entity/RoomThread.kt
@@ -5,15 +5,15 @@ import jakarta.persistence.FetchType
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.OneToOne
-import school.project.soulmate.member.entity.Member
+import school.project.soulmate.stomp.entity.ChatRoom
 import java.time.LocalDate
 
 @Entity
-class Thread(
+class RoomThread(
     @Id
     @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
-    var member: Member,
+    @JoinColumn(name = "chatroom_id")
+    var room: ChatRoom,
     val threadId: String,
     val createDate: LocalDate,
 )
---
+++ src/main/kotlin/school/project/soulmate/gptApi/entity/UserThread.kt
@@ -0,0 +1,19 @@
+package school.project.soulmate.gptApi.entity
+
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.OneToOne
+import school.project.soulmate.member.entity.Member
+import java.time.LocalDate
+
+@Entity
+class UserThread(
+    @Id
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    var member: Member,
+    val threadId: String,
+    val createDate: LocalDate,
+)

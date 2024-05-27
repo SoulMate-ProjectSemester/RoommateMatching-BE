b55e8ad5590897fcaac10f5a3f4bbd17ffebd3c4
High-Quality-Coffee <125748258+High-Quality-Coffee@users.noreply.github.com>
2024-05-27 19:09:33 +0900

    port 번호 테스트 수정 10차

--- src/main/resources/static/js/keywordSelect.js
+++ src/main/resources/static/js/keywordSelect.js
@@ -59,7 +59,8 @@ document.addEventListener('DOMContentLoaded', () => {
                     const responseData = response.data.resultCode;
                     if(responseData=='SUCCESS'){
                         //키워드 저장 성공시, 메인페이지로 넘어가는 로직
-                        window.location.href="http://soulmate.pe.kr";
+                        // window.location.href="http://soulmate.pe.kr";
+                        instance.get("/api/member/main");
                     }
                 }).catch(error => {
                     // Handle errors if the Promise is rejected

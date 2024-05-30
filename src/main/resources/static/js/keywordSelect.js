6c78cc5c4ca184ccda72c1d1325b7aa8e1983bd7
High-Quality-Coffee <125748258+High-Quality-Coffee@users.noreply.github.com>
2024-05-30 20:41:22 +0900

    local 환경에서 권한 테스트 4차

--- src/main/resources/static/js/keywordSelect.js
+++ src/main/resources/static/js/keywordSelect.js
@@ -60,7 +60,6 @@ document.addEventListener('DOMContentLoaded', () => {
                     console.log(response);
                     // 응답 상태 코드를 통해 성공 여부 확인
                     if (response.status === 200) {
-                        // 리디렉션 처리
                         console.log('키워드 저장 성공');
                     } else {
                         alert('키워드 저장에 실패했습니다.');

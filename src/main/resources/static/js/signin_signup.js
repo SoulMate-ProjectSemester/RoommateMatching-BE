ee5968635d15e75ca6875d2b2043faf4f847d88e
High-Quality-Coffee <125748258+High-Quality-Coffee@users.noreply.github.com>
2024-06-01 02:06:22 +0900

    port 수정 2차

--- src/main/resources/static/js/signin_signup.js
+++ src/main/resources/static/js/signin_signup.js
@@ -63,12 +63,12 @@ async function login() {
                         const keywordSetLength=response.data.data.keywordSet;
                         //로그인을 성공했지만, 키워드 입력을 하지 않은 최초로그인, null인 경우
                         if(keywordSetLength===null){
-                            window.location.href="43.201.214.57/api/page/keyword";
+                            window.location.href="/api/page/keyword";
                         }
                         //로그인을 성공했고, 키워드 입력을 미리 한사람 (기존 가입자), null이 아닌경우
                         else{
                             //키워드 입력페이지로 이동
-                            window.location.href='43.201.214.57/api/page/main';
+                            window.location.href='/api/page/main';
                         }
                     }).catch(error=>{
                         console.log('error occurred:',error);

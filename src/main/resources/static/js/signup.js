ce91fc4ba36a1aeb24489dd1359e968bad27ce38
High-Quality-Coffee <125748258+High-Quality-Coffee@users.noreply.github.com>
2024-06-16 00:27:09 +0900

    주석 처리 해제, 반복 회원가입 삭제

--- src/main/resources/static/js/signup.js
+++ src/main/resources/static/js/signup.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
     $('#signupBtn').click(function() {
-        for(let i=0;i<100;i++){
-                signup();
-        }
+        signup();
     });
 });
 async function signup() {
@@ -47,7 +45,7 @@ async function signup() {
         const responseData = error.response ? error.response.data : null;
         if(responseData && responseData.resultCode === "ERROR") {
             // 기본 에러 메시지 표시
-            //alert(`입력값이 잘못되었습니다`);
+            alert(`입력값이 잘못되었습니다`);
             // 구체적인 에러 정보가 있다면 추가로 표시
             if(responseData.data) {
                 // responseData.data가 객체인 경우
@@ -57,15 +55,15 @@ async function signup() {
                         const cleanedKey = key.startsWith('_') ? key.slice(1) : key;
                         return `${cleanedKey} : ${value}`; // 예: "email: 올바른 형식의 이메일 주소여야 합니다"
                     }).join('\n'); // 각 에러 메시지를 줄바꿈 문자로 구분하여 하나의 문자열로 합칩니다.
-                    //alert(`\n${errorMessages}`);
+                    alert(`\n${errorMessages}`);
                 } else {
                     // responseData.data가 기본 타입이거나 문자열인 경우, 직접 표시
-                    //alert(`\n${responseData.data}`);
+                    alert(`\n${responseData.data}`);
                 }
             }
         } else {
             // 기타 에러 처리
-           //console.error("회원가입 중 에러:", error);
+           console.error("회원가입 중 에러:", error);
         }
     }
 }
